7cf240f6dc31f479d117291721bbd3e54f37f8ee
vishnu.interbit <vishnu.interbitsolutions.com>
2022-11-28 00:06:27 +0530

    sadfasdf

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js (github pages by IBS vishnu singh)</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
